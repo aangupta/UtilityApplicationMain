5e83668d14ca2050b3e2d9645ea10e02d450b58b
aangupta <aanchalgupta457@gmail.com>
2021-02-19 22:20:00 +0530

    Added the timer functionality

--- timer.js
+++ timer.js
@@ -1,29 +1,33 @@
 const start = document.querySelector('#start');
 const reset = document.querySelector('#reset');
 
-let h = document.querySelector('#hour');
-let m = document.querySelector('#minute');
-let s = document.querySelector('#second');
+const h = document.querySelector('#hour');
+const m = document.querySelector('#minute');
+const s = document.querySelector('#second');
 
 let startTimer;
+
+function timer() {
+    if (h.value === '0' && m.value === '0' && s.value === '0') {
+        h.value = 0;
+        m.value = 0;
+        s.value = 0;
+    } else if (s.value > '0') {
+        s.value -= 1;
+    } else if (m.value > '0' && s.value === '0') {
+        m.value -= 1;
+        s.value = 60;
+    } else if (h.value > 0 && m.value === '0'){
+        h.value -= 1;
+        m.value = 60;
+    }
+}
 
 function stopTimer() {
     clearInterval(startTimer);
 }
 start.addEventListener('click', () => {
-    startTimer = setInterval(() => {
-        if (s.value === 0) {
-            if (m.value > 0) {
-                s.value = 59;
-                m.value -= 1;
-            }
-        } else {
-            s.value -= 1;
-            if (s.value === 0) {
-                stopTimer();
-            }
-        }
-    }, 1000);
+    startTimer = setInterval(timer, 1000);
 });
 
 reset.addEventListener('click', () => {
