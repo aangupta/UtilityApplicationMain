9380ce6e40db4c3b56ef7e0bea681cd22b4315f8
aangupta <aanchalgupta457@gmail.com>
2021-03-16 00:56:31 +0530

    modified the design

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ promise
     .then((response) => response.json())
     .then((json) => {
         document.querySelector('#ipaddress').value = json.ip;
+        document.querySelector('#city').value = json.city;
+        document.querySelector('#region').value = json.region;
+        document.querySelector('#timezone').value = json.timezone;
+        document.querySelector('#country').value = json.country_name;
+        document.querySelector('#postalcode').value = json.postal;
+        document.querySelector('#currency').value = json.currency;
     });
